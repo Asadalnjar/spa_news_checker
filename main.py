4542183dba6d9796be45add8ea4bbc423f29b586
ASADALDIN ABDULFATTAH <95292493+Asadalnjar@users.noreply.github.com>
2025-07-30 02:25:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ import re
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 print("✅ main.py started", flush=True)
 
